6058c9071fadf38dc7aa52ac0cafdea126d8d9aa
lblazek92 <luka_blazek@hotmail.com>
2014-04-01 11:00:25 +0200

    drugi
    druga verzija

--- Uto1-2014/Form1.Designer.cs
+++ Uto1-2014/Form1.Designer.cs
@@ -28,12 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Polumjer = new System.Windows.Forms.Label();
+            this.Površina = new System.Windows.Forms.Label();
+            this.Opseg = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.IstRezultati = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(94, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Izračunaj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(94, 188);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Obriši";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Polumjer
+            // 
+            this.Polumjer.AutoSize = true;
+            this.Polumjer.Location = new System.Drawing.Point(28, 33);
+            this.Polumjer.Name = "Polumjer";
+            this.Polumjer.Size = new System.Drawing.Size(50, 13);
+            this.Polumjer.TabIndex = 2;
+            this.Polumjer.Text = "Polumjer:";
+            this.Polumjer.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Površina
+            // 
+            this.Površina.AutoSize = true;
+            this.Površina.Location = new System.Drawing.Point(28, 59);
+            this.Površina.Name = "Površina";
+            this.Površina.Size = new System.Drawing.Size(51, 13);
+            this.Površina.TabIndex = 3;
+            this.Površina.Text = "Površina:";
+            // 
+            // Opseg
+            // 
+            this.Opseg.AutoSize = true;
+            this.Opseg.Location = new System.Drawing.Point(28, 87);
+            this.Opseg.Name = "Opseg";
+            this.Opseg.Size = new System.Drawing.Size(41, 13);
+            this.Opseg.TabIndex = 4;
+            this.Opseg.Text = "Opseg:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "0.00";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(81, 56);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "0.00";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(81, 84);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "0.00";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(81, 125);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(88, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Spremi u listu";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // IstRezultati
+            // 
+            this.IstRezultati.FormattingEnabled = true;
+            this.IstRezultati.Location = new System.Drawing.Point(220, 30);
+            this.IstRezultati.Name = "IstRezultati";
+            this.IstRezultati.Size = new System.Drawing.Size(122, 173);
+            this.IstRezultati.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(368, 239);
+            this.Controls.Add(this.IstRezultati);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Opseg);
+            this.Controls.Add(this.Površina);
+            this.Controls.Add(this.Polumjer);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Glavna forma aplikacije";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label Polumjer;
+        private System.Windows.Forms.Label Površina;
+        private System.Windows.Forms.Label Opseg;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ListBox IstRezultati;
     }
 }
 
--- Uto1-2014/Form1.cs
+++ Uto1-2014/Form1.cs
@@ -16,5 +16,25 @@ namespace Uto1_2014
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
